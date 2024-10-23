082f1c8307bb53eea2c751bc90e03a823ca27fb8
Bartosz-Przybylowski <157103437+Bartosz-Przybylowski@users.noreply.github.com>
2024-10-23 13:38:42 +0100

    Add files via upload

---
+++ lekcja/polaczenie_z_baza.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; //github -> mariadb
+$database = "klasa2L";
+
+//polaczenie z serwerem mysql
+mysqli_connect(hostname: $servername, username: $username, password: $password)
+?>
